0ba2ada72572f980dbb8d496965384b2c148f8d1
itggot-erik-dahlgren <38528646+itggot-erik-dahlgren@users.noreply.github.com>
2019-05-09 10:51:02 +0200

    2019-05-09

--- app.rb
+++ app.rb
@@ -3,51 +3,85 @@ require 'slim'
 require 'sqlite3'
 require 'bcrypt'
 require 'byebug'
-require_relative 'fil.rb'
-
+require_relative 'model.rb'
+include MyModule
 enable :sessions
 
+# Display Landing Page
+#
 get ('/') do
     slim(:index)
 end
 
+# Display home page
+#
+# @see MyModule#verify_login
+# @see MyModule#select_products_sql
 get ('/home') do
     if verify_login(session[:id])
-        db = SQLite3::Database.new("db/webshop.db")
-        db.results_as_hash = true
-        
-        products = db.execute("SELECT * FROM products")
+        products = select_products_sql()
         slim(:home, locals:{
             products: products})
     end
 end
 
+# Displays site for creating new user
+#
 get ('/create_user') do
     slim(:create_user)
 end
 
+# Displays site for login into an account
+#
 get ('/login') do
     slim(:login)
 end
 
+# Function to logout of an account and redirects to '/'
+#
 get ('/logout') do
     session.clear
     redirect('/')
 end
 
+# Display an error message
+#
 get ('/error') do
     slim(:error)
 end
 
+# Inputs params to log into an account
+#
+# @param [String] name, The name of an account
+# @param [String] password, The password of an account
+#
+# @see MyModule#login
 post ('/login') do
     login(params["name"], params["password"])
 end
 
+# Creates a new user and redirects to '/'
+#
+# @param [String] re_password, re-entry of password
+# @param [String] password, The password of an account
+# @param [String] name, The name of an account
+# @param [String] email, The email of an account
+#
+# @see MyModule#create_user
 post ('/create_user/new') do
     create_user(params["re_password"], params["password"], params["name"], params["email"])
     redirect('/')
 end
 
+# Creates a post and redirects to '/home'
+#
+# @param [Integer] price, The price of a product
+# @param [Integer] stock, The amount of a product in stock
+# @param [String] title, The title of a post
+# @param [String] picture, The picture url of a product
+#
+# @see MyModule#verify_login
+# @see MyModule#post_article
 post ('/post_article') do
     if verify_login(session[:id])
         post_article(session[:id], params["price"], params["stock"], params["title"], params["picture"])
@@ -55,6 +89,11 @@ post ('/post_article') do
     end
 end
 
+# Deletes a post and redirects to '/home'
+#
+# @param [Integer] id, The id of an account
+#
+# @see MyModule#post_delete
 post ('/home/:id/delete') do
     post_delete(params["id"])
     redirect('/home')
---
+++ model.rb
@@ -0,0 +1,66 @@
+module MyModule
+    # Attempts to create a new user
+    #
+    # @option params [String] username The username
+    # @option params [String] password The password
+    # @option params [String] repeat_password The repeated password
+    def create_user(re_password, password, name, email)
+        db = SQLite3::Database.new("db/webshop.db")
+        db.results_as_hash = true
+        
+        if re_password != password
+            redirect('/')
+        end
+
+        pass = BCrypt::Password.create(password)
+        if db.execute("SELECT name FROM custumers WHERE name=?", name) != true
+            db.execute("INSERT INTO custumers (name, password, email) VALUES (?,?,?)", name, pass, email)
+        end
+    end
+
+    def login(name, password)
+        db = SQLite3::Database.new("db/webshop.db")
+        db.results_as_hash = true
+        
+        result = db.execute("SELECT id, name, password, email FROM custumers WHERE name = ?", name)
+        if BCrypt::Password.new(result.first["password"]) == password
+            session[:name] = result.first["name"]
+            session[:id] = result.first["id"]
+            session[:email] = result.first["email"]
+            redirect("/home")
+        else
+            redirect('/login')
+        end
+    end
+
+    def verify_login(id)
+        if id == nil
+            redirect('/error')
+        else
+            true
+        end
+    end
+
+    def post_article(user_id, price, stock, title, picture)
+        db = SQLite3::Database.new("db/webshop.db")
+        db.results_as_hash = true
+
+        db.execute("INSERT INTO products (user_id, price, stock, title, picture) VALUES (?,?,?,?,?)", user_id, price, stock, title, picture)
+    end 
+
+    def post_delete(id)
+        db = SQLite3::Database.new("db/webshop.db")
+        db.results_as_hash = true
+        user_id = db.execute("SELECT user_id FROM products WHERE id = ?", id)
+        if user_id.first["user_id"] == session[:id]
+            db.execute("DELETE FROM products WHERE products.id = ?", id)
+        end
+    end
+
+    def select_products_sql()
+        db = SQLite3::Database.new("db/webshop.db")
+        db.results_as_hash = true
+        products = db.execute("SELECT * FROM products")
+    end
+
+end
